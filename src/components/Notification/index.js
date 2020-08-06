16369d7d5733759df61add2d98b0dec42d4e3fde
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-08-06 12:59:52 +0300

    hw-02 task-01

---
+++ src/components/Notification/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Notification = ({message}) =>{
+    return(
+        <h3>{message}</h3>
+    )
+}
+
+Notification.propTypes = {
+    message: PropTypes.string
+}
+
+export default Notification;
---
+++ src/components/Section/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Section = ({title,children}) =>{
+    return(
+        <>
+        <h2>{title}</h2>
+        {children}
+        </>
+    )
+}
+
+Section.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.element.isRequired
+
+}
+
+export default Section
